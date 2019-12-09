a11bf2f579f4d8a2901f7780a15814215c302ac4
Dmytro Korzhevin <dkorzhevin@gmail.com>
2019-12-09 13:36:01 +0200

    Add some react js sample code

---
+++ react/react.js
@@ -0,0 +1,8 @@
+// npm install react
+// npm  install react-dom
+
+import React from ‘react’
+import ReactDOM from ‘react-dom’
+
+const title = React.createElement(‘h1’ ,{ } , ‘First React Element’ );
+ReactDOM.render()
