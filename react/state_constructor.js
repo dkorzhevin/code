7e07946b4ed50d15369769a828f2dac298f03d84
Dmytro Korzhevin <dkorzhevin@gmail.com>
2019-12-09 15:02:18 +0200

    Add more react js code

---
+++ react/state_constructor.js
@@ -0,0 +1,11 @@
+Class Sample extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = { attribute : "value" };
+	}
+}
+
+//<h1> Hello {this.state.name}!</h1>
+//<p> {this.state.message}</p>
