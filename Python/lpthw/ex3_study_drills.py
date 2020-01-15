99e2e698744cf2cec80802fb5e05eaf89e50a76e
Dmytro Korzhevin <dkorzhevin@gmail.com>
2020-01-15 17:13:23 +0200

    Add more files

---
+++ Python/lpthw/ex3_study_drills.py
@@ -0,0 +1,43 @@
+# Start of script
+
+# print regular string
+print("I will now count my chickens:")
+
+# print result of expression
+print("Hens", 25 + 30 / 6)
+
+# print text string + result of expression
+print("Roosters", 100 - 25 * 3 % 4)
+
+# print string
+print("Now I will count the eggs:")
+
+# print result of expression
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# string + result
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# result of expression
+print(3 + 2 < 5 - 7)
+
+# string + result of expression
+print("What is 3 + 2?", 3 + 2)
+
+# string + result of expression
+print("What is 5 - 7?", 5 - 7)
+
+# print string
+print("Oh, that's why it's False.")
+
+# print string
+print("How about some more.")
+
+# print string + expression result
+print("Is it greater?", 5 > -2)
+
+# print string + expression result
+print("Is it greater or equal?", 5 >= -2)
+
+# print string + result
+print("Is it less or equal?", 5 <= -2)
