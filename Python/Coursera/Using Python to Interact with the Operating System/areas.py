d3f5bca40af0db81c87972b8e062613b55018a9f
Dmytro Korzhevin <dkorzhevin@gmail.com>
2020-01-24 13:36:21 +0200

    Add more code

---
+++ Python/Coursera/Using Python to Interact with the Operating System/areas.py
@@ -0,0 +1,11 @@
+import math
+
+def triangle(base, height):
+    return base*height/2
+
+def rectangle(base, height):
+    return base*height
+
+def circle(radius):
+    return math.pi*(radius**2)
+
---
+++ Python/Coursera/Using Python to Interact with the Operating System/working_with_modules.py
@@ -0,0 +1,11 @@
+# Importing our own module (areas.py)
+import areas
+
+# Triangle
+print("Triangle area: ", areas.triangle(3,5))
+
+# Circle
+print("Circle area: ", areas.circle(4))
+
+# Rectangle
+print("Rectangle area: ", areas.rectangle(3, 6))
