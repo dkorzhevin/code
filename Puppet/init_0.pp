7d7ece2a45329e0102f7e0f6a73d93a81876efed
Dmytro Korzhevin <dkorzhevin@gmail.com>
2020-02-04 18:35:07 +0200

    Add basic init file

---
+++ Puppet/init_0.pp
@@ -0,0 +1,10 @@
+# sudo puppet agent -v --test
+
+class profile {
+        file { '/etc/profile.d/append-path.sh':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0644',
+                content => "PATH=\$PATH:/java/bin\n",
+        }
+}
